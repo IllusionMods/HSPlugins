IllusionMods/HSPlugins

commit a95ee8a0a59b301ce613c26aafc8f82888206aab

Match OCIItems with their index instead of their order by index (#95)
I'm not sure why this was done when a perfectly good index value was available but it gives the plugin room to break, and it did when I made ObjectMap which adds negative values to the dictionary thus causing the order to shift.

Core.PoseEditor/MainWindow.cs
@@ -282,7 +282,7 @@ namespace HSPE
             else
                 AdvancedModeModule._repeatTimer = 0f;
             AdvancedModeModule._repeatCalled = false;
-            
+
             BoneReorganizer.Update();
         }
 
@@ -1611,7 +1611,7 @@ namespace HSPE
                 return;
             this.ExecuteDelayed2(() =>
             {
-                LoadSceneGeneric(node, new SortedDictionary<int, ObjectCtrlInfo>(Studio.Studio.Instance.dicObjectCtrl).ToList());
+                LoadSceneGeneric(node, Studio.Studio.Instance.dicObjectCtrl);
             }, 3);
         }
 
@@ -1628,7 +1628,7 @@ namespace HSPE
             Dictionary<int, ObjectCtrlInfo> toIgnore = new Dictionary<int, ObjectCtrlInfo>(Studio.Studio.Instance.dicObjectCtrl);
             this.ExecuteDelayed2(() =>
             {
-                LoadSceneGeneric(node, Studio.Studio.Instance.dicObjectCtrl.Where(e => toIgnore.ContainsKey(e.Key) == false).OrderBy(e => SceneInfo_Import_Patches._newToOldKeys[e.Key]).ToList());
+                LoadSceneGeneric(node, Studio.Studio.Instance.dicObjectCtrl.Where(e => toIgnore.ContainsKey(e.Key) == false).ToDictionary(d => d.Key, m => m.Value));
             }, 3);
         }
 
@@ -1638,17 +1638,38 @@ namespace HSPE
         /// <param name="node"></param>
         public void ExternalLoadScene(XmlNode node)
         {
-            LoadSceneGeneric(node, new SortedDictionary<int, ObjectCtrlInfo>(Studio.Studio.Instance.dicObjectCtrl).ToList());
+            LoadSceneGeneric(node, Studio.Studio.Instance.dicObjectCtrl);
         }
 
-        private void LoadSceneGeneric(XmlNode node, List<KeyValuePair<int, ObjectCtrlInfo>> dic)
+        private void LoadSceneGeneric(XmlNode node, Dictionary<int, ObjectCtrlInfo> dic)
         {
             if (node == null || node.Name != "root")
                 return;
-            string v = node.Attributes["version"].Value;
-            int i = 0;
+
             foreach (XmlNode childNode in node.ChildNodes)
             {
+                if (childNode.Name.Equals("itemInfo") == false)
+                {
+                    continue;
+                }
+
+                var attribute = childNode.Attributes?["index"];
+
+                if (attribute == null)
+                {
+                    continue;
+                }
+
+                if (int.TryParse(attribute.Value, out var index) == false)
+                {
+                    continue;
+                }
+
+                if (dic[index] is OCIItem ociItem)
+                {
+                    LoadElement(ociItem, childNode);
+                }
+                /*
                 switch (childNode.Name)
                 {
                     case "itemInfo":
@@ -1657,10 +1678,12 @@ namespace HSPE
                             ++i;
                         if (i == dic.Count)
                             break;
+                        HSPE.Logger.LogMessage($"{i} matched {dic[i].Value.objectInfo.dicKey}, if this isn't right, kys.");
                         LoadElement(ociItem, childNode);
                         ++i;
                         break;
                 }
+                */
             }
         }
 

commit 121a525de252338bf437b1c5574d318adb9d72b5

Fix imports losing data caused by #95

Core.PoseEditor/MainWindow.cs
@@ -1611,7 +1611,9 @@ namespace HSPE
                 return;
             this.ExecuteDelayed2(() =>
             {
-                LoadSceneGeneric(node, Studio.Studio.Instance.dicObjectCtrl);
+                LoadSceneGeneric(node,
+                    Studio.Studio.Instance.dicObjectCtrl,
+                    Studio.Studio.Instance.sceneInfo.dicChangeKey?.ToDictionary(m => m.Value, d => d.Key));
             }, 3);
         }
 
@@ -1628,7 +1630,9 @@ namespace HSPE
             Dictionary<int, ObjectCtrlInfo> toIgnore = new Dictionary<int, ObjectCtrlInfo>(Studio.Studio.Instance.dicObjectCtrl);
             this.ExecuteDelayed2(() =>
             {
-                LoadSceneGeneric(node, Studio.Studio.Instance.dicObjectCtrl.Where(e => toIgnore.ContainsKey(e.Key) == false).ToDictionary(d => d.Key, m => m.Value));
+                LoadSceneGeneric(node,
+                    Studio.Studio.Instance.dicObjectCtrl.Where(e => toIgnore.ContainsKey(e.Key) == false).ToDictionary(d => d.Key, m => m.Value),
+                    Studio.Studio.Instance.sceneInfo.dicChangeKey?.ToDictionary(m => m.Value, d => d.Key));
             }, 3);
         }
 
@@ -1638,10 +1642,12 @@ namespace HSPE
         /// <param name="node"></param>
         public void ExternalLoadScene(XmlNode node)
         {
-            LoadSceneGeneric(node, Studio.Studio.Instance.dicObjectCtrl);
+            LoadSceneGeneric(node,
+                Studio.Studio.Instance.dicObjectCtrl,
+                Studio.Studio.Instance.sceneInfo.dicChangeKey?.ToDictionary(m => m.Value, d => d.Key));
         }
 
-        private void LoadSceneGeneric(XmlNode node, Dictionary<int, ObjectCtrlInfo> dic)
+        private void LoadSceneGeneric(XmlNode node, Dictionary<int, ObjectCtrlInfo> dic, Dictionary<int, int> changeDictionary)
         {
             if (node == null || node.Name != "root")
                 return;
@@ -1665,7 +1671,22 @@ namespace HSPE
                     continue;
                 }
 
-                if (dic[index] is OCIItem ociItem)
+                retryInfoGet:
+                
+                var item = dic.GetValueSafe(index);
+                
+                if (item == null)
+                {
+                    if (changeDictionary != null && changeDictionary.TryGetValue(index, out var newIndex))
+                    {
+                        index = newIndex;
+                        goto retryInfoGet;
+                    }
+                    HSPE.Logger.LogWarning($"Item at key {index} could not be found. PE data will not be loaded for it.");
+                    continue;
+                }
+
+                if (item is OCIItem ociItem)
                 {
                     LoadElement(ociItem, childNode);
                 }