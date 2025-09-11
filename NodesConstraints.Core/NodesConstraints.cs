9ad660b47db985b5552ac1ba4745ce2a5f1aeae6
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-09-11 19:35:16 +0200

    Bump version numbers

--- NodesConstraints.Core/NodesConstraints.cs
+++ NodesConstraints.Core/NodesConstraints.cs
@@ -52,7 +52,7 @@ namespace NodesConstraints
     {
         public const string Name = "NodesConstraints";
         public const string GUID = "com.joan6694.illusionplugins.nodesconstraints";
-        public const string Version = "1.5";
+        public const string Version = "1.6";
 #if KOIKATSU || AISHOUJO || HONEYSELECT2
         private const string _extSaveKey = "nodesConstraints";
         private const int _saveVersion = 0;
