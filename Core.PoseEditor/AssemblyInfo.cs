8e64d5a19d91befb4b62fece79ae42654df3917e
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2026-01-02 00:23:58 +0100

    Revert "Fix broken repo links that pointed to GH"
    This reverts commit 59e4e11a7203ce9a42a8595d2b02f59f921e7719.

--- Core.PoseEditor/AssemblyInfo.cs
+++ Core.PoseEditor/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-using System.Reflection;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle(HSPE.HSPE.Name)]
 [assembly: AssemblyDescription("Pose Editor for Studio")]
-[assembly: AssemblyCompany("https://gitgoon.dev/IllusionMods/HSPlugins")]
+[assembly: AssemblyCompany("https://github.com/IllusionMods/HSPlugins")]
 [assembly: AssemblyProduct(HSPE.HSPE.Name)]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyVersion(HSPE.HSPE.Version)]
--- Timeline.Core/AssemblyInfo.cs
+++ Timeline.Core/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-using System.Reflection;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle(Timeline.Timeline.Name)]
 [assembly: AssemblyDescription("Keyframe sequencing/animation tool for Studio")]
-[assembly: AssemblyCompany("https://gitgoon.dev/IllusionMods/HSPlugins")]
+[assembly: AssemblyCompany("https://github.com/IllusionMods/HSPlugins")]
 [assembly: AssemblyProduct(Timeline.Timeline.Name)]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyVersion(Timeline.Timeline.Version)]
